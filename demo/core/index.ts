49d82d0df9b3efb6edab9c1a8c3cbd73f983055d
Andy Coenen <andycoenen@google.com>
2019-07-29 15:42:54 -0700

    Update projection to use umap projected data

--- demo/core/index.ts
+++ demo/core/index.ts
@@ -1,38 +1,24 @@
-import * as fmnist from '../../data/fmnist.json';
+import * as fmnist from '../../data/projection.json';
 
 import { DataPoint, DataSet } from '../../src/data';
 
 class State {
-  data: number[][] = [];
-  labels: number[] = fmnist.labels;
-  labelNames = new Map<number, string>();
-
   // Projector-compatible data points wrapper for visualization
   dataSet: DataSet;
 
   constructor() {
-    // Populate the label names
-    for (let i = 0; i < fmnist.label_names.length; i++) {
-      this.labelNames.set(i, fmnist.label_names[i]);
-    }
-
-    const dataPoints: DataPoint[] = [];
-    for (let i = 0; i < fmnist.data.length; i++) {
-      // Flatten 2D array of fashion mnist data
-      const fmnistItem: number[][] = fmnist.data[i];
-      const vector = fmnistItem.reduce((prev, curr) => prev.concat(curr), []);
-      this.data.push(vector);
-
-      const label = this.labelNames.get(this.labels[i]) || '';
-
-      dataPoints.push({
-        vector: new Float32Array(fmnist.projection[i]),
-        metadata: {
-          label,
-        },
-        index: i,
-      });
-    }
+    const dataPoints: DataPoint[] = fmnist.projection.map(
+      (vector: number[], index) => {
+        const label = fmnist.labels[index];
+        return {
+          vector,
+          metadata: {
+            label: fmnist.label_names[label],
+          },
+          index,
+        };
+      }
+    );
 
     this.dataSet = new DataSet(dataPoints, 3);
   }

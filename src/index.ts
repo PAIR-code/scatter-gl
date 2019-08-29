6da1eff6bfd1dc50ef5f0974cdb6579f8823b962
Andy Coenen <andycoenen@google.com>
2019-08-29 15:54:09 -0400

    Export types from index.ts

--- src/index.ts
+++ src/index.ts
@@ -14,4 +14,5 @@ limitations under the License.
 ==============================================================================*/
 
 export {ScatterGL, ScatterGLParams} from './scatter_gl';
+export {Dataset, Points, PointMetadata, Sequence, SpriteMetadata} from './data';
 export {InteractionMode, RenderMode, Vector, Point2D, Point3D} from './types';
